58d7454404428d06d56d813e0669befd48db2e6e
techcomkathia <techcomk@gmail.com>
2024-11-05 18:34:09 -0300

    atividade desafio

--- AULA02/atividade06.js
+++ AULA02/atividade06.js
@@ -1,6 +1,7 @@
 // Implemente uma aplicação simples que simule as operações básicas de uma conta bancária. 
 let saldo = 1000
 let divida = false
+let emprestimo = 0
 
 
 //  A aplicação deve ter as seguintes funcionalidades: 
@@ -9,8 +10,8 @@ let divida = false
 //  depósito (2)
 //       vai adicionar o valor  ao saldo
 //  empréstimo (3)
-//       trocar o booleano da divida e adicionar o valor do empréstimo ao saldo
-//  extrato. (4)
+//       trocar o booleano da divida, caso seja FALSE, o usuario vai poder realizar o empréstimo. Será adicionadoo valor do empréstimo ao saldo e colocado como negativo no emprestimo
+//  extrato (4)
 //       mostrar o saldo e as dividas da conta
 
 
