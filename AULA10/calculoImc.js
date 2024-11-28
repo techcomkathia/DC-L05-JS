a74afec9a6a6b18022d0bfd8afc2c6b225993a51
techcomkathia <techcomk@gmail.com>
2024-11-28 20:03:58 -0300

    selecção dos elementos do DOM com querySelector

---
+++ AULA10/calculoImc.js
@@ -0,0 +1,17 @@
+// ------------lógica da questão-----------------
+// peso (pedido do usuário)
+let peso = Number(prompt('Qual seu peso? (kg)'))
+// altura (pedido do usuário)
+let altura = Number(prompt('Qual sua altura? (m)'))
+// imc ---> calculado
+let imc = peso / (altura ** 2)
+// -----------------------------------------------
+
+// ----------manipular o DOM----------------------
+// div id =peso
+// div id =altura
+
+// p id= pesoInfo
+// p id= alturaInfo
+// p id= imcInfo
+// -----------------------------------------------
