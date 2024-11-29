2003f17e193c4bcf906a2e3c19f5d34dd3564482
techcomkathia <techcomk@gmail.com>
2024-11-28 21:34:02 -0300

    selecção dos elementos do DOM com querySelector

--- AULA10/calculoImc.js
+++ AULA10/calculoImc.js
@@ -9,9 +9,24 @@ let imc = peso / (altura ** 2)
 
 // ----------manipular o DOM----------------------
 // div id =peso
-// div id =altura
+    let divPeso = document.querySelector('#peso')
 
+// div id =altura
+    let divAltura = document.querySelector('#altura')
 // p id= pesoInfo
+    let pPesoInfo = document.querySelector('#pesoInfo')
 // p id= alturaInfo
+    let pAlturaInfo = document.querySelector('#alturaInfo')
 // p id= imcInfo
+    let pImcInfo = document.querySelector('#imcInfo')
+
+
+    divPeso.innerHTML = `<h3>${peso}</h3>`
+    divAltura.innerHTML = `<h3>${altura}</h3>`
 // -----------------------------------------------
+
+    pPesoInfo.innerHTML = `Peso: ${peso}`
+
+    pAlturaInfo.innerHTML = `Altura: ${altura}`
+
+    pImcInfo.innerHTML = `IMC: ${imc}`
