ca4b48fea99a84d8648ea7b3a68417d5dc5b4435
techcomkathia <techcomk@gmail.com>
2025-03-27 19:54:11 -0300

    heranca

--- NODE/aula30/contas.js
+++ NODE/aula30/contas.js
@@ -9,7 +9,38 @@
 // numero
 // saldo (será privado)
 
+class ContaBancaria { //classe mãe, superclasse ou classe base
+    // abstração e encapsulamento
+    #saldo
 
-// crie duas classes derivadas: ContaCorrente e ContaPoupanca
-// conta corrente deve ter um atributo limiteEmprestimo
-// conta poupanca deve ter um atributo percentualRendimento
+    constructor( numeroDaConta,agencia, titular, saldo ){
+        this.numeroDaConta = numeroDaConta
+        this.agencia = agencia
+        this.titular = titular
+        this._saldo = saldo
+    }
+
+    depositar( valor ){ 
+        this._saldo += valor
+        console.log(`Depósito realizado com sucesso. Seu saldo atual é ${this._saldo}`) 
+    }
+
+    sacar( valor ){
+        if(this._saldo >= valor){
+            this._saldo -= valor
+            console.log(`Saque realizado com sucesso. Seu saldo atual é ${this._saldo}`)
+        }
+        else{console.log('Saldo insuficiente')}
+    }
+
+    consultarSaldo(){
+        console.log(this._saldo)
+    }
+
+}
+
+
+// crie duas classes derivadas: 
+// ContaCorrente contará com o atributo limiteEmprestimo
+
+// ContaPoupanca deve ter um atributo percentualRendimento
