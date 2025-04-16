cba21753e94702f0de4e42d56132f24e37f2452a
techcomkathia <techcomk@gmail.com>
2025-04-15 21:53:02 -0300

    post livros

--- NODE/aula35/services/LivrosService.js
+++ NODE/aula35/services/LivrosService.js
@@ -23,6 +23,54 @@ function getLivros(req, res){
         })
 }
 
+async function postLivro(req, res) {
+    const {titulo, isbn, preco, publicado_em, estoque, categoria_id, autor_id} = req.body
+
+    try{
+        //verificar se o livro existe
+        const livroPesquisa = await LivrosModel.findOne({where:{titulo:titulo}})
+
+        //se o livro não existir, verificar se todos os campos foram preenchidos, se todos forem preenchidos, criar o livro
+        if(!livroPesquisa){
+            const livroCriado = await LivrosModel.create(
+                {
+                    titulo:titulo,
+                    isbn:isbn, preco:preco,
+                    publicado_em:publicado_em,
+                    estoque:estoque,
+                    categoria_id:categoria_id,
+                    autor_id:autor_id
+                }
+            )
+
+            res.json({
+                status: 200,
+                dadosCriados: livroCriado
+            })
+        }
+        else{
+            res.json({
+                status: 400,
+                message: 'Livro ja cadastrado'
+            })
+        }        //caso contrário, retornar uma mensagem de erro
+
+        
+    }
+    catch(erro){
+        res.status(500).json(
+            {
+                message: erro,
+                status: 500,
+                erro: 'Erro ao criar o livro'
+            }
+        )
+    }
+}
+
+
+
 module.exports = {
-    getLivros
+    getLivros,
+    postLivro
 }
